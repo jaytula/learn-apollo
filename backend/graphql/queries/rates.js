ba8eca9553758f22fc37c72a53e6278c5c198e2f
Jay T <jaytula@gmail.com>
2020-05-22 23:49:08 -0700

    Adjust backend graphql rates query

--- backend/graphql/queries/rates.js
+++ backend/graphql/queries/rates.js
@@ -1,20 +1,26 @@
-const { gql } = require("apollo-server-express");
+const { gql } = require('apollo-server-express');
 
 exports.typeDefs = gql`
   extend type Query {
-    rates(currency: String!): CurrencyData!
+    rates(currency: String!): [CurrencyData]!
   }
 
   type CurrencyData {
     currency: String!
+    rate: Float!
   }
 `;
 
 exports.resolvers = {
   Query: {
     rates: async (parent, { currency }) => {
-      const rateData = { USD: 1, THB: 0.04 };
-      return { currency: `${currency}: ${rateData[currency]}` };
+      const rateData = { USD: 1, KRW: 0.08, THB: 0.04 };
+      return Object.keys(rateData)
+        .filter((key) => key !== currency)
+        .map((key) => ({
+          currency: key,
+          rate: rateData[key] / rateData[currency],
+        }));
     },
   },
 };
